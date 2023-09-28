74e385b560b8ea649e13b1e2545ac22f762f144b
Zach Leatherman <zachleatherman@gmail.com>
2023-09-28 13:16:50 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+# `table-saw`
+
+A small structural-only Web Component for responsive `<table>` elements. Heavily inspired by [Filament Group’s Tablesaw Stack jQuery plugin](https://github.com/filamentgroup/tablesaw).
+
+## Example
+
+```html
+<!-- Note: requires `type="module"` -->
+<script type="module" src="table-saw.js"></script>
+
+<table-saw>
+	<table>
+		<!-- some HTML omitted for brevity -->
+	</table>
+</table-saw>
+```
+
+Supports one or many `<table>` child elements.
+
+## API
+
+* Use `media` attribute to set the breakpoint (default `(max-width: 39.9375em)`, e.g. `<table-saw media="(max-width: 24.9375em)">`).
+* Use `ratio` attribute to override the small viewport column ratio (default `1/2`, e.g. `<table-saw ratio="1/1">`).
+
+## Features
+
+* Uses CSS grid for small viewport alignment.
+* Falls back to regular table before or without JavaScript.
+* Cuts the mustard on [`CSSStyleSheet.prototype.replaceSync`](https://caniuse.com/mdn-api_cssstylesheet_replacesync)
---
+++ table-saw.js
@@ -0,0 +1,146 @@
+class Tablesaw extends HTMLElement {
+	constructor() {
+		super();
+
+		this.autoOffset = 50;
+
+		this.attrs = {
+			breakpoint: "media",
+			ratio: "ratio",
+		};
+
+		this.defaults = {
+			breakpoint: '(max-width: 39.9375em)', // same as Filament Group’s Tablesaw
+			ratio: '1fr 2fr',
+		};
+
+		this.classes = {
+			active: "active",
+		};
+
+		this.props = {
+			ratio: "--table-saw-ratio",
+		};
+
+		this.css = `table-saw.active thead td,
+table-saw.active thead th {
+	position: absolute;
+	height: 1px;
+	width: 1px;
+	overflow: hidden;
+	clip: rect(1px, 1px, 1px, 1px);
+}
+table-saw.active tbody tr,
+table-saw.active tfoot tr {
+	display: block;
+	margin-bottom: 1em;
+}
+table-saw.active tbody td,
+table-saw.active tbody th,
+table-saw.active tfoot td,
+table-saw.active tfoot th {
+	display: grid;
+	gap: 0 1em;
+	grid-template-columns: var(--table-saw-ratio, ${this.defaults.ratio});
+}
+
+table-saw .table-saw-label {
+	display: none;
+}
+table-saw.active .table-saw-label {
+	display: revert !important;
+}`;
+	}
+
+	connectedCallback() {
+		// Cut the mustard
+		// https://caniuse.com/mdn-api_cssstylesheet_replacesync
+		if(!("replaceSync" in CSSStyleSheet.prototype)) {
+			return;
+		}
+
+		this.init();
+
+		// we *could* pass { media: this.getAttribute(this.attrs.breakpoint) } in
+		// but then the breakpoints would be global and we want per-table breakpoints
+		let sheet = new CSSStyleSheet();
+		sheet.replaceSync(this.css);
+
+		// Only add stylesheet once
+		if(document.adoptedStyleSheets?.[0]?.cssRules?.[0]?.cssText !== sheet.cssRules[0].cssText) {
+			document.adoptedStyleSheets.push(sheet);
+		}
+	}
+
+	init() {
+		let added = this.addHeaders();
+
+		if(added === false) {
+			console.error("Could not find any `<th>` elements for Tablesaw web component.", this);
+			return;
+		}
+
+		this.setRatio();
+
+		let breakpoint = this.getAttribute(this.attrs.breakpoint) || this.defaults.breakpoint;
+
+		Tablesaw.onmedia(breakpoint, (matches) => {
+			this.classList[matches ? "add" : "remove"](this.classes.active);
+		});
+	}
+
+	static onmedia(query, callback) {
+		let mm = {
+			matches: true
+		};
+
+		if(query && ("matchMedia" in window)) {
+			mm = window.matchMedia(query);
+		}
+
+		if(mm.matches) {
+			callback(true);
+		}
+
+		mm.addListener(e => {
+			callback(e.matches);
+		});
+	}
+
+	addHeaders() {
+		let labels = Array.from(this.querySelectorAll("thead th")).map(entry => entry.innerText);
+		if(labels.length === 0) {
+			return false;
+		}
+
+		let nodes = [];
+		for(let label of labels) {
+			let l = document.createElement("b");
+			l.classList.add("table-saw-label");
+			l.setAttribute("aria-hidden", true);
+			l.style.display = "none"; // defensive in case the stylesheet insertion fails
+			l.innerText = label;
+
+			nodes.push(l);
+		}
+
+		let cells = this.querySelectorAll("tbody td");
+		for(let cell of cells) {
+			cell.prepend(nodes[cell.cellIndex].cloneNode(true));
+		}
+	}
+
+	setRatio() {
+		let ratio = this.getAttribute(this.attrs.ratio);
+		if(ratio) {
+			let ratioString = ratio.split("/").join("fr ") + "fr";
+			this.style.setProperty(this.props.ratio, ratioString);
+		}
+	}
+}
+
+if("customElements" in window) {
+	window.customElements.define("table-saw", Tablesaw);
+}
+
+export { Tablesaw };
